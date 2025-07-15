c0418f080e5780713780359b7a7398a7b2d02eb8
MajidAbuRmila <majid@controlplane.com>
2025-07-16 01:21:00 +0300

    Revert last change in the previous commit

--- src/prefect/runner/runner.py
+++ src/prefect/runner/runner.py
@@ -508,7 +508,6 @@ class Runner:
                             workload=workload,
                             interval=self.query_seconds,
                             jitter_range=0.3,
-                            run_once=True,
                         )
                     )
 
